ad870ce51b290ec9632c4489014b1bbf9df2d3fd
edwarv <edwarvilchez1977@gmail.com>
2026-02-01 00:57:18 -0400

    security: restringido acceso a pagos solo para roles administrativos en el sidebar

--- client/src/app/components/shared/sidebar/sidebar.ts
+++ client/src/app/components/shared/sidebar/sidebar.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LanguageService } from '../../../services/language.service';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
-  constructor(public langService: LanguageService) {}
+  constructor(
+    public langService: LanguageService,
+    public authService: AuthService
+  ) {}
 }
